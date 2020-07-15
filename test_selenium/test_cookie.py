91e5d2636eb5869c11b7904283882a0eb1ebdfa2
elsa-liu <m13381368355@163.com>
2020-07-15 19:29:13 +0800

    learn cookie in CT

--- test_selenium/test_cookie.py
+++ test_selenium/test_cookie.py
@@ -2,15 +2,19 @@ import json
 import time
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class TestCookie:
     def setup(self):
         self.driver = webdriver.Chrome()
-        self.driver.get("https://work.weixin.qq.com/wework_admin/loginpage_wx?from=myhome")
+        #self.driver.get("https://work.weixin.qq.com/wework_admin/loginpage_wx?from=myhome")
+        self.driver.get("https://work.weixin.qq.com/wework_admin/frame")
 
     def test_get_cookie(self):
-        # time.sleep(10)
+        time.sleep(10)
         cookies = self.driver.get_cookies()
         with open("cookie.json", "w") as f:
             json.dump(cookies, f)
@@ -18,10 +22,25 @@ class TestCookie:
     def test_cookie_login(self):
         cookies = json.load(open("cookie.json"))
         for cookie in cookies:
-            # 遍历整个cookies列表,拿到里面的字典,把cookie键值对一个一个塞入浏览器中
+            if 'expiry' in cookie.keys():
+                del cookie['expiry']
             self.driver.add_cookie(cookie)
-        time.sleep(10)
-        self.driver.refresh()
-
+        while True:
+            self.driver.refresh()
+            res = WebDriverWait(self.driver,10).until(expected_conditions.element_to_be_clickable
+                                                ((By.ID,"menu_index")))
+            if res is not None:
+                break
+        #expected_condition.xx()  都是需要传入一个数组
+        WebDriverWait(self.driver,10).until(expected_conditions.element_to_be_clickable
+                                            ((By.CSS_SELECTOR,'.index_service_cnt_itemWrap:nth-child(2)')))
+        self.driver.find_element(By.CSS_SELECTOR,'.index_service_cnt_itemWrap:nth-child(2)').click()
+        WebDriverWait(self.driver,10).until(expected_conditions.presence_of_element_located
+                                            ((By.ID,"js_upload_file_input")))
+        self.driver.find_element(By.ID,"js_upload_file_input")\
+            .send_keys(r"C:\Users\Administrator\PycharmProjects\lagou2QProject\test_selenium\data\work_contact.xls")
+        res = self.driver.find_element(By.ID,'upload_file_name').text
+        time.sleep(3)
+        assert res=="work_contact.xls"
     def teardown(self):
         self.driver.quit()
