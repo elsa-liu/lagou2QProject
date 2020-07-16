b551c8cdfca0ee0e60e028dffd1a61d5a19bb623
elsa-liu <m13381368355@163.com>
2020-07-16 15:46:52 +0800

    this is homework for python1

--- pythoncode/demo.py
+++ pythoncode/demo.py
@@ -1,1 +1,21 @@
-# this is demo for git
+#this is homework for python1
+
+#定义有参数的函数,下面函数的参数为必需参数
+def have_arg(a):
+    b = a+1
+    #有返回值 返回值为b
+    return b
+
+#定义无参数的函数
+def no_arg():
+    print("this is no argument function")
+    #无返回值情况 默认返回值为None
+    return
+
+
+print(have_arg(1))
+#输出值为2
+
+print(no_arg())
+#输出值为“this is no argument”
+# None
